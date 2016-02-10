ef84731949c34ee169c8b134b5d508ebfafe927f
Gandhi Mena Salas <gandhimenasalas@macbook-pro-de-gandhi.local>
2016-02-09 23:03:56 -0600

    asignación de ViewControllers a mis vistas

---
+++ My_beginner_Spotify/DetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsViewController.swift
+//  My_beginner_Spotify
+//
+//  Created by Gandhi Mena Salas on 09/02/16.
+//  Copyright © 2016 Trenx. All rights reserved.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
